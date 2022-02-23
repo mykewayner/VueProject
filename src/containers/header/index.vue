0300f97c6ee568d40de153fed69202b1f6cfa45d
mykewayner <mykewayner@gmail.com>
2022-02-23 01:43:54 -0300

    Feat: Created header container

---
+++ src/containers/header/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <header>
+        <img src="@/src/assets/Logo-Compasso-Branco-hor1.png"/>
+
+    </header>
+  
+</template>
+
+<script>
+export default {
+    name: 'Header',
+    component: {
+
+    }
+
+}
+</script>
+
+<style>
+
+</style>
