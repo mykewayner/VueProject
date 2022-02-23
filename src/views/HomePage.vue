3d50cf9bcc8b3d0203d3a14c672f0e573320b320
mykewayner <mykewayner@gmail.com>
2022-02-23 01:43:28 -0300

    Fix: Renaming homeview to homepage

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -2,17 +2,17 @@
   <div class="home">
     
     
+    
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import home from '@/components/home.vue'
 
 export default {
   name: 'HomeView',
   components: {
-   home
+  
   }
 }
 </script>
