5b36d62decb8925c84c238390aa1cd6e793c747b
mykewayner <mykewayner@gmail.com>
2022-02-24 05:34:14 -0300

    Feat: added text content component

---
+++ src/components/textContent/index.vue
@@ -0,0 +1,35 @@
+<template>
+   <p :class="{homeText}">{{ text }}</p>
+</template>
+
+<script>
+export default {
+    name: 'textContent',
+    props: {
+        text: {
+            type: String,
+            required: true,
+            default: "Default Text",
+        },
+        homeText: {
+            default: false,
+        },
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
+</script>
+
+<style lang="scss" scoped>
+@import './index.scss';
+</style>
--- src/containers/homecontent/index.vue
+++ src/containers/homecontent/index.vue
@@ -1,26 +1,20 @@
 <template>
     <div id="mainContent">
         <img draggable="false" src="@/assets/bola-LogoCompasso1.png" />
-        <div id="homeText">
-            <div>
-              <Title class="title" firstHomeTitle=true level='3' text="Our mission is"/>
+        <div id="homeTextBox">
             
-            </div>
+            <Title class="title" firstHomeTitle=true level='3' text="Our mission is"/>
+            <textContent homeText=true text="que permitam o crecimento dos nossos clientes"/>
+                      
+            <Title class="title" homeTitle=true level='1' text="to transform the world"/>
+            <textContent homeText=true text="que permitam o crecimento dos nossos clientes"/>
+                      
+            <Title class="title" homeTitle=true level='1' text="building digital experiences"/>
+            <textContent homeText=true text="que permitam o crecimento dos nossos clientes"/>
+                            
+            <Title class="title" homeTitle=true level='1' text="that enable our client's growth"/>
+            <textContent homeText=true text="que permitam o crecimento dos nossos clientes"/>
             
-            <div>
-                <Title class="title" homeTitle=true level='1' text="to transform the world"/>
-            
-            </div>
-
-            <div>
-                <Title class="title" homeTitle=true level='1' text="building digital experiences"/>
-            
-            </div>
-               
-            <div>
-               <Title class="title" homeTitle=true level='1' text="that enable our client's growth"/>
-            
-            </div>
         </div>
         
         
@@ -29,11 +23,13 @@
 </template>
 
 <script>
-import Title from '@/components/title'
+import Title from '@/components/title/index.vue'
+import textContent from '@/components/textContent/index.vue'
 export default {
     name: "HomeContent",
     components:{
-        Title
+        Title,
+        textContent,
 
     }
 
