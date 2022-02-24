8dc09f7ed2345b21ca6f9f1db26a641acfd4143a
mykewayner <mykewayner@gmail.com>
2022-02-23 21:38:02 -0300

    Feat: Created main home content container

---
+++ src/containers/homecontent/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="mainContent">
+        <img draggable="false" src="@/assets/bola-LogoCompasso1.png" />
+        <div id="homeText"></div>
+        
+        
+    </div>
+  
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+@import './index.scss';
+</style>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="home">
+    <Header></Header>
+    <homecontent></homecontent>
+
     
     
     
@@ -8,10 +11,14 @@
 
 <script>
 // @ is an alias to /src
+import Header from "@/containers/header/index.vue"
+import homecontent from "@/containers/homecontent/index.vue"
 
 export default {
-  name: 'HomeView',
+  name: 'HomePage',
   components: {
+    Header,
+    homecontent
   
   }
 }
