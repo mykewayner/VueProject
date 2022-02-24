ea35053bcfdbec3d61913e7998de3b4ac200d421
mykewayner <mykewayner@gmail.com>
2022-02-24 03:41:22 -0300

    Feat: Creating a title component

---
+++ src/components/title/index.vue
@@ -0,0 +1,38 @@
+<template>
+    <h1 v-if="level === '1'" :class="{homeTitle, firstHomeTitle}"> {{ text }}</h1>
+    <h2 v-else-if="level === '2'" :class="{homeTitle, firstHomeTitle}">{{ text }}</h2>
+    <h3 v-else-if="level === '3'" :class="{homeTitle, firstHomeTitle}">{{ text }}</h3>
+    <h4 v-else-if="level === '4'" :class="{homeTitle, firstHomeTitle}">{{ text }}</h4>
+    <h5 v-else-if="level === '5'" :class="{homeTitle, firstHomeTitle}">{{ text }}</h5>
+    <h6 v-else-if="level === '6'" :class="{homeTitle, firstHomeTitle}">{{ text }}</h6>
+</template>
+
+<script>
+export default {
+ name:'Title',
+ props:{
+   text:{
+     type: String,
+     required: true,
+     default: 'Default title'
+   },
+   level:{
+     type: String,
+     required: true,
+     default: '1'
+   },
+   firstHomeTitle:{
+     default: false,
+   },
+   homeTitle:{
+     default: false,
+   },
+ },
+
+ 
+}
+</script>
+
+<style lang='scss' scoped>
+    @import './index.scss';
+</style>
--- src/containers/homecontent/index.vue
+++ src/containers/homecontent/index.vue
@@ -1,7 +1,23 @@
 <template>
     <div id="mainContent">
         <img draggable="false" src="@/assets/bola-LogoCompasso1.png" />
-        <div id="homeText"></div>
+        <div id="homeText">
+            <div>
+              <Title class="title" firstHomeTitle=true level='3' text="Our mission is"/>
+            </div>
+            
+            <div>
+                <Title class="title" homeTitle=true level='1' text="to transform the world"/>
+            </div>
+
+            <div>
+                <Title class="title" homeTitle=true level='1' text="building digital experiences"/>
+            </div>
+               
+            <div>
+               <Title class="title" homeTitle=true level='1' text="that enable our client's growth"/>
+            </div>
+        </div>
         
         
     </div>
@@ -9,7 +25,13 @@
 </template>
 
 <script>
+import Title from '@/components/title'
 export default {
+    name: "HomeContent",
+    components:{
+        Title
+
+    }
 
 }
 </script>
