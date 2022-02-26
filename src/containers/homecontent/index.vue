01740c058c659e4a4191bbd781477ed9c0c7d382
mykewayner <mykewayner@gmail.com>
2022-02-26 17:22:42 -0300

    Style: Changed text styles

--- src/containers/homecontent/index.vue
+++ src/containers/homecontent/index.vue
@@ -2,19 +2,22 @@
     <div id="mainContent">
         <img draggable="false" src="@/assets/bola-LogoCompasso1.png" />
         <div id="homeTextBox">
-            
+            <div>
             <Title class="title" firstHomeTitle=true level='3' text="Our mission is"/>
             <textContent homeText=true text="que permitam o crecimento dos nossos clientes"/>
-                      
+            </div>
+            <div>          
             <Title class="title" homeTitle=true level='1' text="to transform the world"/>
             <textContent homeText=true text="que permitam o crecimento dos nossos clientes"/>
-                      
+            </div>
+            <div>         
             <Title class="title" homeTitle=true level='1' text="building digital experiences"/>
             <textContent homeText=true text="que permitam o crecimento dos nossos clientes"/>
-                            
+            </div>
+            <div>              
             <Title class="title" homeTitle=true level='1' text="that enable our client's growth"/>
             <textContent homeText=true text="que permitam o crecimento dos nossos clientes"/>
-            
+            </div>
         </div>
         
         
