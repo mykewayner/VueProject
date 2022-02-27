73c3ca5b6fb6fca2ea9d1e3e6423af8d7cd0e0c5
mykewayner <mykewayner@gmail.com>
2022-02-27 20:41:47 -0300

    Feat: Created base component for the login pagin

---
+++ src/containers/containerLeft/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="containerLeftContent"><p>Macaco</p></div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style  lang="scss" scoped>
+@import './index.scss';
+</style>
