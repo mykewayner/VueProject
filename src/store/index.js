db0d6aaeeab52e3a9bce6bd0e30ad3ad0fab85dd
mykewayner <mykewayner@gmail.com>
2022-03-01 19:05:05 -0300

    feat: Finished creating clock component

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,20 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import router from '@/router'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    logged: false,
   },
   getters: {
   },
   mutations: {
+    logout(state){
+      state.logged = false
+      router.push('/login')
+    },
   },
   actions: {
   },
