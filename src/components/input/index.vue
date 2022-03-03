29df90786cd62f2693c9b422449775ad7ecf147e
mykewayner <mykewayner@gmail.com>
2022-03-03 03:45:54 -0300

    Feat: Created login logic

--- src/components/input/index.vue
+++ src/components/input/index.vue
@@ -1,13 +1,17 @@
 <template>
-  <input :type="type" :placeholder="placeholder" :class="{textInput, submitInput}">  
+    <div>
+        <input v-model="user" @input="handleUser" v-if="type === 'text'" :type="type" :placeholder="placeholder" :class="{textInput}">
+        <input v-model="password" @input="handlePassword" v-if="type === 'password'" :type="type" :placeholder="placeholder" :class="{textInput}">
+        <input v-if="type === 'submit'" :type="type" :placeholder="placeholder" :class="{submitInput}">
+    </div>  
 </template>
 
 <script>
+import {mapMutations, mapState} from 'vuex'
 export default {
     name: "Input",
     props:{
         type:{
-            required: true,
             default: "text",
             type: String,
         },
@@ -25,6 +29,28 @@ export default {
         }
 
     },
+    data(){
+        return{
+            user:"",
+            password:"",
+        }
+    },
+    computed:{
+    
+      ...mapState(["userData", "passwordData"])
+    
+    },
+    methods: {
+      ...mapMutations(["loginAuth", "setUser","setPassword"]),
+      handleUser(){
+        this.$store.commit("setUser", this.user)
+      },
+      handlePassword(){
+        this.$store.commit("setPassword", this.password)
+      }
+      
+          
+    }
 }
 </script>
 
