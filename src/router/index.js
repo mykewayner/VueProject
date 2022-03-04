fecaa11312c7e5fdad0075fbf7328bb410bb5002
mykewayner <mykewayner@gmail.com>
2022-03-04 02:07:32 -0300

    Feat: added dynamic title change

--- src/router/index.js
+++ src/router/index.js
@@ -6,19 +6,29 @@ Vue.use(VueRouter)
 
 const routes = [
   {
+    path: '*',
+    redirect: '/login'
+  },
+  {
+    path: '/',
+    redirect: '/login'
+  },
+  {
     path: '/home',
     name: 'home',
     component: HomePage,
+    meta: { title: 'Compass - Home' }
 
 
   },
   {
-    path: '/',
+    path: '/login',
     name: 'login',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '@/views/Login/LoginPage.vue'),
+    meta: { title: 'Compass - Login Page' }
 
   }
 ]
@@ -33,5 +43,13 @@ router.beforeEach((to, from, next)=>{
   if ( to.name === 'home' && !store.state.logged ){next({name: 'login',})}
   else next()
 })
+const DEFAULT_TITLE = 'Some Default Title';
+router.afterEach((to) => {
+    // Use next tick to handle router history correctly
+    // see: https://github.com/vuejs/vue-router/issues/914#issuecomment-384477609
+    Vue.nextTick(() => {
+        document.title = to.meta.title || DEFAULT_TITLE;
+    });
+});
 
 export default router
